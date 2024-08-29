3066d509d510d54f255113ecf42629a4fe048756
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-29 13:10:59 +0100

    ♻️ Refactor Game1: Implement Result interface in DrawResult and make parallel change for DeterminateDrawResult

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,91 +1,111 @@
-namespace Tennis
+namespace Tennis;
+
+public class TennisGame1 : ITennisGame
 {
-    public class TennisGame1 : ITennisGame
+    private int _player1Score = 0;
+    private int _player2Score = 0;
+    private string Player1Name;
+    private string Player2Name;
+
+    public TennisGame1(string player1Name, string player2Name)
     {
-        private int _player1Score = 0;
-        private int _player2Score = 0;
-        private string Player1Name;
-        private string Player2Name;
+        Player1Name = player1Name;
+        Player2Name = player2Name;
+    }
 
-        public TennisGame1(string player1Name, string player2Name)
+    public void WonPoint(string playerName)
+    {
+        if (playerName == "player1")
+            _player1Score++;
+        else
+            _player2Score++;
+    }
+
+    public string GetScore()
+    {
+        string score = "";
+        var tempScore = 0;
+        if (IsDraw())
         {
-            Player1Name = player1Name;
-            Player2Name = player2Name;
+            return DeterminateDrawResult();
         }
 
-        public void WonPoint(string playerName)
+        if (_player1Score >= 4 || _player2Score >= 4)
         {
-            if (playerName == "player1")
-                _player1Score++;
-            else
-                _player2Score++;
+            return AdvantageOrWinResult();
         }
 
-        public string GetScore()
+        return ResultPerPoint(score);
+    }
+
+    private string ResultPerPoint(string score)
+    {
+        return GetScoreAsString(_player1Score) + "-" + GetScoreAsString(_player2Score);
+    }
+
+    private static string GetScoreAsString(int score)
+    {
+        switch (score)
         {
-            string score = "";
-            var tempScore = 0;
-            if (IsDraw())
-            {
-                return DrawResult();
-            }
-
-            if (_player1Score >= 4 || _player2Score >= 4)
-            {
-                return AdvantageOrWinResult();
-            }
-
-            return ResultPerPoint(score);
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
         }
 
-        private string ResultPerPoint(string score)
-        {
-            return GetScoreAsString(_player1Score) + "-" + GetScoreAsString(_player2Score);
-        }
+        return score.ToString();
+    }
 
-        private static string GetScoreAsString(int score)
-        {
-            switch (score)
-            {
-                case 0:
-                    return "Love";
-                case 1:
-                    return "Fifteen";
-                case 2:
-                    return "Thirty";
-                case 3:
-                    return "Forty";
-            }
+    private string AdvantageOrWinResult()
+    {
+        string score;
+        var minusResult = _player1Score - _player2Score;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
 
-            return score.ToString();
-        }
+    private string DeterminateDrawResult()
+    {
+        return new DrawResult(_player1Score, _player2Score).GetScoreAsText();
+    }
 
-        private string AdvantageOrWinResult()
-        {
-            string score;
-            var minusResult = _player1Score - _player2Score;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-            return score;
-        }
-
-        private string DrawResult()
-        {
-            return _player1Score switch
-            {
-                0 => "Love-All",
-                1 => "Fifteen-All",
-                2 => "Thirty-All",
-                _ => "Deuce",
-            };
-        }
-
-        private bool IsDraw()
-        {
-            return _player1Score == _player2Score;
-        }
+    private bool IsDraw()
+    {
+        return _player1Score == _player2Score;
     }
 }
 
+public interface Result
+{
+    public string GetScoreAsText();
+}
+
+public class DrawResult : Result
+{
+    private int Player1Score;
+    private int Player2Score;
+
+    public DrawResult(int player1Score, int player2Score)
+    {
+        Player1Score = player1Score;
+        Player2Score = player2Score;
+    }
+
+    public string GetScoreAsText()
+    {
+        return Player1Score switch
+        {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            _ => "Deuce",
+        };
+    }
+}
