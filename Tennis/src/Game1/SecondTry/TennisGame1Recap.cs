97f619e52ad2a3ba0579e4a78ec2c197f90ddce5
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-30 09:30:46 +0100

    ♻️ Refactor: Extract methods in GetScore

--- Tennis/src/Game1/SecondTry/TennisGame1Recap.cs
+++ Tennis/src/Game1/SecondTry/TennisGame1Recap.cs
@@ -24,58 +24,81 @@ public class TennisGame1Recap : ITennisGame
     public string GetScore()
     {
         string score = "";
-        var tempScore = 0;
         if (m_score1 == m_score2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+            score = DeterminateDrawResult();
         }
         else if (m_score1 >= 4 || m_score2 >= 4)
         {
-            var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            score = DeterminateAdvantageOrWinResult();
         }
         else
         {
-            for (var i = 1; i < 3; i++)
-            {
-                if (i == 1) tempScore = m_score1;
-                else { score += "-"; tempScore = m_score2; }
-                switch (tempScore)
-                {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
-            }
+            score = DeterminateOngoingResult(score);
         }
         return score;
+    }
+
+    private string DeterminateOngoingResult(string score)
+    {
+        var tempScore = 0;
+        for (var i = 1; i < 3; i++)
+        {
+            if (i == 1) tempScore = m_score1;
+            else { score += "-"; tempScore = m_score2; }
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+        }
+
+        return score;
+    }
+
+    private string DeterminateAdvantageOrWinResult()
+    {
+        string score;
+        var minusResult = m_score1 - m_score2;
+        if (minusResult == 1)
+        {
+            score = "Advantage player1";
+        }
+        else if (minusResult == -1)
+        {
+            score = "Advantage player2";
+        }
+        else if (minusResult >= 2)
+        {
+            score = "Win for player1";
+        }
+        else
+        {
+            score = "Win for player2";
+        }
+
+        return score;
+    }
+
+    private string DeterminateDrawResult()
+    {
+        return m_score1 switch
+        {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            _ => "Deuce",
+        };
     }
 }
 
