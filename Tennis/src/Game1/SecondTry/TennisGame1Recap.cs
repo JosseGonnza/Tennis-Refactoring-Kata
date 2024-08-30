956fc4880b330cf28dbe6026fcdd125532286f4c
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-30 21:22:17 +0100

    ✨ Implement IResult in DrawResult

--- Tennis/src/Game1/SecondTry/TennisGame1Recap.cs
+++ Tennis/src/Game1/SecondTry/TennisGame1Recap.cs
@@ -97,4 +97,25 @@ public interface IResult
     public string GetScoreAsText();
 }
 
+public class DrawResult : IResult
+{
+    private int PlayerOneScore;
+    private int PlayerTwoScore;
 
+    public DrawResult(int playerOneScore, int playerTwoScore)
+    {
+        PlayerOneScore = playerOneScore;
+        PlayerTwoScore = playerTwoScore;
+    }
+
+    public string GetScoreAsText()
+    {
+        return PlayerOneScore switch
+        {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            _ => "Deuce",
+        };
+    }
+}
