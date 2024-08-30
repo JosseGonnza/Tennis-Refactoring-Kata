d0ee4b60bcec6aa082b4500b9104faf5bf2a06f5
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-30 22:25:39 +0100

    ♻️ Refactor: Split AdvantageOrWin in two use case

--- Tennis/src/Game1/SecondTry/ArbiterTwo.cs
+++ Tennis/src/Game1/SecondTry/ArbiterTwo.cs
@@ -1,4 +1,5 @@
-﻿using Tennis.src.Game1.SecondTry.Models;
+﻿using System;
+using Tennis.src.Game1.SecondTry.Models;
 using Tennis.src.Game1.SecondTry.Results;
 
 namespace Tennis.src.Game1.SecondTry;
@@ -11,13 +12,27 @@ class ArbiterTwo
         {
             return new DrawResultTwo(player1Score, player2Score);
         }
-        else if (player1Score >= 4 || player2Score >= 4)
+        else if (AnyPlayerHasAdvantage(player1Score, player2Score) && SomeIsAheadByOne(player1Score, player2Score))
         {
-            return new AdvantageOrWinResultTwo(player1Score, player2Score);
+            return new AdvantageResultTwo(player1Score, player2Score);
+        }
+        else if (AnyPlayerHasAdvantage(player1Score, player2Score))
+        {
+            return new WinResultTwo(player1Score, player2Score);
         }
         else
         {
             return new OngoingResultTwo(player1Score, player2Score);
         }
+    }
+
+    private static bool SomeIsAheadByOne(int player1Score, int player2Score)
+    {
+        return Math.Abs(player1Score - player2Score) == 1;
+    }
+
+    private static bool AnyPlayerHasAdvantage(int player1Score, int player2Score)
+    {
+        return (player1Score >= 4 || player2Score >= 4);
     }
 }
---
+++ Tennis/src/Game1/SecondTry/Results/AdvantageResultTwo.cs
@@ -0,0 +1,37 @@
+﻿using Tennis.src.Game1.SecondTry.Models;
+
+namespace Tennis.src.Game1.SecondTry.Results;
+
+public class AdvantageResultTwo : ResultTwo
+{
+    private int PlayerOneScore;
+    private int PlayerTwoScore;
+
+    public AdvantageResultTwo(int playerOneScore, int playerTwoScore)
+    {
+        PlayerOneScore = playerOneScore;
+        PlayerTwoScore = playerTwoScore;
+    }
+
+    public string GetScoreAsText()
+    {
+        return PlayerOneScore > PlayerTwoScore ? "Advantage player1" : "Advantage player2";
+    }
+}
+
+public class WinResultTwo : ResultTwo
+{
+    private int PlayerOneScore;
+    private int PlayerTwoScore;
+
+    public WinResultTwo(int playerOneScore, int playerTwoScore)
+    {
+        PlayerOneScore = playerOneScore;
+        PlayerTwoScore = playerTwoScore;
+    }
+
+    public string GetScoreAsText()
+    {
+        return PlayerOneScore > PlayerTwoScore ? "Win for player1" : "Win for player2";
+    }
+}
